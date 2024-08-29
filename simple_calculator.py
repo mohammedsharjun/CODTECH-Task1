d9dd61944b9e2006470c117f7f1d649f9f233d43
mohammedsharjun <dhanishsharjun9@gmail.com>
2024-08-29 19:31:29 +0530

    first commit

---
+++ simple_calculator.py
@@ -0,0 +1,28 @@
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1/2/3/4): ")
+
+if choice in ('1', '2', '3', '4'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", num1 + num2)
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", num1 - num2)
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", num1 * num2)
+
+    elif choice == '4':
+        if num2 != 0:
+            print(num1, "/", num2, "=", num1 / num2)
+        else:
+            print("Error! Division by zero is not allowed.")
+else:
+    print("Invalid Input")
